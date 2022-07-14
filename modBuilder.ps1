cb1ba2895734b9c836d5bbad7c4abdf561ec6cbe
Jimmy Nielsen <fragithub@silverdragons.dk>
2022-07-14 16:55:33 +0200

    first files

---
+++ modBuilder.ps1
@@ -0,0 +1,8 @@
+$include = @("*.xml", "*.lua", "*.dds", "src\vehicles\specialization\*.lua", "src\*.lua", "translations\*.xml")
+$zipFilename = "FS22_PlowHalfTurn.zip"
+
+if(test-path "$env:ProgramFiles\WinRAR\WinRAR.exe")
+{
+    Set-Alias winrar "$env:ProgramFiles\WinRAR\WinRar.exe"
+    Start-Process -wait -FilePath winrar -ArgumentList "a -afzip $zipFilename $include"
+}
