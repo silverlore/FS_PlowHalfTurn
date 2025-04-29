3c8110cc11fd1c215838fc4687a40de56690414b
Silverlore <fragithub@silverdragons.dk>
2025-04-29 17:50:41 +0200

    change to support fs25

--- modBuilder_test.ps1
+++ modBuilder_test.ps1
@@ -1,9 +1,9 @@
 $include = @("*.xml", "*.lua", "*.dds", "src\vehicles\specialization\*.lua", "src\vehicles\specialization\event\*.lua", "src\*.lua", "translations\*.xml")
-$zipFilename = "FS22_PlowHalfTurn.zip"
+$zipFilename = "FS25_PlowHalfTurn.zip"
 
 if(test-path "$env:ProgramFiles\WinRAR\WinRAR.exe"){
     Set-Alias winrar "$env:ProgramFiles\WinRAR\WinRar.exe"
     Start-Process -wait -FilePath winrar -ArgumentList "a -afzip $zipFilename $include"
 
-    Copy-Item .\$zipFilename $home'\Documents\my games\FarmingSimulator2022\mods\'
+    Copy-Item .\$zipFilename $home'\Documents\my games\FarmingSimulator2025\mods\'
 }
